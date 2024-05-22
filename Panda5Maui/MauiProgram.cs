508c76b01b8a5e3c3fda85e61294f8dbcad6ea87
Kamil Kwiatkowski <kamilkwiatkos@gmail.com>
2024-05-22 22:50:36 +0200

    Implemented simple calendar

--- Panda5Maui/MauiProgram.cs
+++ Panda5Maui/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace Panda5Maui
 {
@@ -9,6 +10,7 @@ namespace Panda5Maui
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
---
+++ Panda5Maui/ViewModels/CalendarViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCalendar.Core.Models;
+
+namespace Panda5Maui.ViewModels;
+
+internal class CalendarViewModel : ObservableObject
+{
+    public Calendar<CalendarDay> Calendar { get; set; } = new Calendar<CalendarDay>();
+
+    public CalendarViewModel()
+    {
+    }
+}
