6daf9aa301a541e80aa2c8a7e789ea8ca2310c7f
Kamil Kwiatkowski <kamilkwiatkos@gmail.com>
2024-05-30 17:24:46 +0200

    Selecting days on calendar

--- Panda5Maui/ViewModels/CalendarViewModel.cs
+++ Panda5Maui/ViewModels/CalendarViewModel.cs
@@ -4,15 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using XCalendar.Core.Models;
 
 namespace Panda5Maui.ViewModels;
 
 internal class CalendarViewModel : ObservableObject
 {
-    public Calendar<CalendarDay> Calendar { get; set; } = new Calendar<CalendarDay>();
+    public Calendar<CalendarDay> Calendar { get; set; } = new Calendar<CalendarDay>()
+    {
+        SelectionAction = XCalendar.Core.Enums.SelectionAction.Modify,
+        SelectionType = XCalendar.Core.Enums.SelectionType.Single
+    };
+
+    public Command DaySelect { get; }
 
     public CalendarViewModel()
     {
+        DaySelect = new Command<DateTime>((Day) =>
+        {
+            Calendar.SelectedDates.Clear();
+            Calendar.NavigatedDate = Day;
+            Calendar.SelectedDates.Add(Day);
+        });
     }
 }
